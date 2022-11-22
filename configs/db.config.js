1eb28458587b463ecf74619ffa9750f7921e2b19
winthepooh <lord.poohfaca@gmail.com>
2022-11-22 21:32:53 +0700

    final try

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  HOST: "remotemysql.com",
-  USER: "d3WCguLKpM",
-  PASSWORD: "BuN29iUfTU",
-  DB: "d3WCguLKpM",
+  HOST: "containers-us-west-93.railway.app",
+  USER: "root",
+  PASSWORD: "zCcTbXz0dxZ693eyhr4G",
+  DB: "railway",
+  PORT: 6638,
 };
